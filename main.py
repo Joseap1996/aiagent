0a8750a72560f924d54667e61817491c332fc59e
Joseap1996 <joseap1996@live.com>
2025-08-03 18:05:24 -0500

    C: added list to keep track of messages betwen user and the aiagent

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import os
-from dotenv import load_dotenv
 import sys
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
 from google import genai
-client = genai.Client(api_key=api_key)
+from google.genai import types
+from dotenv import load_dotenv
+
 
 def main():
-    if len(sys.argv) > 1:
-        content = sys.argv[1]
-        response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=content
-        )
-        print(response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        print("error, no prompt given")
+    load_dotenv()
+    args = sys.argv[1:]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    messages =[
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client,messages)
+
+
+def generate_content(client,messages):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+    print("Response:")
+    print(response.text)
+
 if __name__ == "__main__":
     main()
