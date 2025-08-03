9cced967dce9b5b03e81b6a4d98c0bc426f0f319
Joseap1996 <joseap1996@live.com>
2025-08-03 17:12:44 -0500

    B:Now accepts command line arguments as prompts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -8,11 +9,16 @@ from google import genai
 client = genai.Client(api_key=api_key)
 
 def main():
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
-    )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if len(sys.argv) > 1:
+        content = sys.argv[1]
+        response = client.models.generate_content(
+            model='gemini-2.0-flash-001', contents=content
+        )
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    else:
+        print("error, no prompt given")
+        sys.exit(1)
 if __name__ == "__main__":
     main()
